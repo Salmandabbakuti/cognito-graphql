70003ef36943c341d7a18c9847a9a6b6a0953268
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-08-03 12:29:17 +0530

    Init (#1)
    * first commit
    
    * Create .env
    
    * Update resolvers.js
    
    * Update typeDefs.js
    
    * Update server.js
    
    Co-authored-by: Salmandabbakuti <Salmandabbakuti@github.com>

---
+++ src/typeDefs.js
@@ -0,0 +1,21 @@
+const typeDefs = `
+type Query {
+  login(username:String!, password:String!):loginResponse!
+}
+type loginResponse{
+    token: String!
+}
+type Mutation {
+  signup(name:String!, email:String!, phone:String!, address:String!, username:String!, password:String!):response!
+  changePassword(oldPassword:String!, newPassword:String!):response!
+  forgotPasswordRequest(username:String!):response!
+  forgotPasswordChange(username:String!, code:String!, newPassword:String!):response!
+  signout:response!
+  confirmEmail(username:String!, code:String!):response!
+
+}
+type response {
+    data: String!
+}`
+
+export default typeDefs;
